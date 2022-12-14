c59281ccc8696bcb019aee21e067780cd6d51a42
kkc217 <kkc217@naver.com>
2022-12-14 12:22:32 +0900

    초기 설정

---
+++ src/test/java/com/capstone/sts/StsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.sts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
